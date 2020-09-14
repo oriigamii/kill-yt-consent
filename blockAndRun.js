601de9a586cc1c9607276ac3071cc2b8f914cd14
oriigamii <mistrnbdy@protonmail.com>
2020-09-14 14:27:49 +0200

    Use queryselector for new bumper

--- blockAndRun.js
+++ blockAndRun.js
@@ -1,10 +1,10 @@
 if (window.location.host == "www.youtube.com") {
-  var tag = document.getElementById("consent-bump");
-  if (tag) {
-    document.getElementById("consent-bump").remove();
+  let bump = document.querySelector('[id*="consent-bump"]');
+  if (bump) {
+    document.getElementById(bump.id).remove();
     setTimeout(function () {
-      var video = document.getElementsByTagName("video")[0];
-      if (video.paused) {
+      let video = document.getElementsByTagName("video")[0];
+      if (video && video.paused) {
         video.play();
       }
     }, 1000)
